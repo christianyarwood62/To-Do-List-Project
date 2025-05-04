4a493986172cd1200606fab94b0564c954df04a4
Christian Yarwood <christianyarwood@gmail.com>
2025-05-04 20:07:33 +0100

    Make an index js file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import "./styles.css";
+import { ToDoItem } from './classes.js';
+import { renderFrontPage } from "./make-front-page.js";
+import { showSideBar } from "./sidebar.js";
+
+renderFrontPage();
+showSideBar();
