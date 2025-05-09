c575d0d8efbb526142acfa655d27e023b8aef6b4
Christian Yarwood <christianyarwood@gmail.com>
2025-05-09 19:50:23 +0100

    Remove old code

--- src/index.js
+++ src/index.js
@@ -1,25 +1,9 @@
 import "./styles.css";
-// import { renderGenericToDoList } from "./page_handlers.js";
+import { PageController } from "./controller.js";
+
 import { showSideBar } from "./sidebar.js";
-import { createContentTemplate } from "./page_handlers.js";
-import { createGenericItemsProject, createProjectsArray } from "./ToDo_item_data_handlers.js";
+import { createGenericItemsProject, createProjectsArray } from "./data.js";
 
-// // Declares an empty array for items in the global scope (i.e. the window)
-// const itemTable = [];
-// if (typeof window !== 'undefined') {
-//     window.itemTable = itemTable;
-// }
 
-// // Declares an empty array for projects in the global scope (i.e. the window)
-// const initialProjectTable = [];
-// if (typeof window !== 'undefined') {
-//     window.initialProjectTable = initialProjectTable;
-// }
-
-// Initialise the front page with the items area
-// Event: Show the generic to do items upon webpage initialisation
-addEventListener("DOMContentLoaded", (event) => {
-    createContentTemplate();
-    showSideBar();
-    createGenericItemsProject();
-})
+const newController = new PageController();
+newController.view.renderListTemplate();
