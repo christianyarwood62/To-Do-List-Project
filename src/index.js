c4f9ff57451fa824ca8d3c377a2bde8a26ee7445
Christian Yarwood <christianyarwood@gmail.com>
2025-05-09 19:58:23 +0100

    Make sidebar hardcoded in html

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import "./styles.css";
 import { PageController } from "./controller.js";
 
-import { showSideBar } from "./sidebar.js";
 import { createGenericItemsProject, createProjectsArray } from "./data.js";
 
 
